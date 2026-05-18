ab73ca829dc5899b30021b991939c581a4c7e9ee
Tilek Orozobekov <tilekusupbekov678@gmail.com>
2026-05-19 01:20:18 +0600

    refactor: make empty commit and push to trigger redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -42,7 +42,6 @@ const App = () => {
   const page = Number(searchParams.get('page')) || 1;
 
   useEffect(() => {
-    
     const loadData = async () => {
       try{
         const currentPage = page ? page : 1;
