ef66ddb5396f349ed32a45ff194d0ba1f13e5f1f
Tilek Orozobekov <tilekusupbekov678@gmail.com>
2026-05-24 13:31:50 +0600

    feat: implement basic download in csv format

--- src/components/Flyout/index.tsx
+++ src/components/Flyout/index.tsx
@@ -9,12 +9,29 @@ const Flyout = () => {
   const selectedAmount = selectedPokemons.length;
   const dispatch = useDispatch();
 
+  const handleDownload = () => {
+    const finalString = selectedPokemons.map((selected) => `${selected.id},${selected.name},${selected.url}`).join('\n');
+    
+    const blob = new Blob([finalString], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.setAttribute('href', url);
+    link.setAttribute('download', `${selectedAmount}_items.csv`);
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.tooltip}>
         <label>selected items: {selectedAmount}</label>
         <Button onClick={() => dispatch(unselectAll())}>Unselect all</Button>
-        <Button>Download</Button>
+        <Button onClick={handleDownload}>Download</Button>
       </div>
     </div>
   );
